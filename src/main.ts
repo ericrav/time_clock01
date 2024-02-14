c362f05556eecee2aeb54e3bb7964648184d1d3e
Eric Rabinowitz <ericrabinowitz@me.com>
2024-02-13 21:02:27 -0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import SunCalc from 'suncalc';
 
 const sketch = (p: p5) => {
   let position: GeolocationPosition;
+  let date = new Date('2024-06-20 7:02 AM');
   p.setup = () => {
     p.createCanvas(800, 800);
 
@@ -14,6 +15,7 @@ const sketch = (p: p5) => {
   };
 
   p.draw = () => {
+    date = new Date(date.getTime() + 1000*10);
     p.translate(p.width / 2, p.height / 2);
     if (!position) return;
     p.background(0);
@@ -23,23 +25,36 @@ const sketch = (p: p5) => {
     const s = 550;
     p.ellipse(0, 0, s, s);
 
-    const times = SunCalc.getTimes(new Date(), position.coords.latitude, position.coords.longitude);
-    const positions = SunCalc.getPosition(new Date(), position.coords.latitude, position.coords.longitude);
-    console.log(times);
-    console.log(positions);
-    const { sunrise, sunset, solarNoon } = times;
-
     p.noStroke();
     p.fill(255);
+    p.text(formatTime(date), 0, 0);
+
+    const times = SunCalc.getTimes(date, position.coords.latitude, position.coords.longitude);
+    const positions = SunCalc.getPosition(date, position.coords.latitude, position.coords.longitude);
+    console.log(times);
+    console.log(positions);
+
+    const { azimuth, altitude } = positions;
+
+    p.text(radiansToDegrees(altitude), 0, 20);
+    p.text(radiansToDegrees(azimuth), 0, 40);
+
+
+    const sunX = -s*0.58 * Math.cos(azimuth - Math.PI / 2);
+    const sunY = -s / 2 * (altitude / (Math.PI / 2));
+    p.ellipse(sunX, sunY, 20, 20);
+
+    const { sunrise, sunset, solarNoon } = times;
+
     p.textSize(21);
-    p.textAlign(p.RIGHT)
+    p.textAlign(p.RIGHT, p.CENTER);
     p.text(formatTime(sunset), -s / 2 - 16, 0);
-    p.textAlign(p.LEFT)
+    p.textAlign(p.LEFT, p.CENTER)
     p.text(formatTime(sunrise), s / 2 + 16, 0);
-    p.textAlign(p.CENTER)
+    p.textAlign(p.CENTER, p.BASELINE)
     p.text(formatTime(solarNoon), 0, - s / 2 - 16);
 
-    p.noLoop();
+    // p.noLoop();
   };
 };
 
@@ -52,3 +67,8 @@ const formatTime = (date: Date) => {
 }
 
 new p5(sketch);
+
+
+function radiansToDegrees(radians: number) {
+  return (radians * 180 / Math.PI).toFixed(2);
+}
